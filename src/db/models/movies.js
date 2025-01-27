4f5e52add057855b1cfcab40d395ab2c00946858
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2025-01-27 21:35:26 +0200

    start

---
+++ src/db/models/movies.js
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose';
+
+const moviesSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    actors: {
+      type: String,
+      required: true,
+    },
+    director: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    releaseDate: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default: null,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+const MovieCollection = model('movie', moviesSchema);
+export default MovieCollection;
---
+++ src/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+// import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  // app.use(
+  //   pino({
+  //     transport: {
+  //       target: 'pino-pretty',
+  //     },
+  //   }),
+  // );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use('*', notFoundHandler);
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
